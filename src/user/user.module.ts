4620f5f31fbc898d953394e9adbe7d1005a0cd33
Jordan Woodruff <jordan@neko-chan.local>
2020-01-29 22:19:46 -0600

    typeorm repository working

---
+++ src/user/user.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { UserService } from './user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../entity/User';
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [UserService],
+})
+export class UserModule {}
