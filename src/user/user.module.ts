1583ab18fbf348650b1363aff3b4ca582024c547
Jordan Woodruff <jordan@neko-chan.local>
2020-01-29 22:23:22 -0600

    add repo

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, InjectRepository } from '@nestjs/typeorm';
 import { User } from '../entity/User';
+import { Repository } from 'typeorm';
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   providers: [UserService],
 })
-export class UserModule {}
+export class UserModule {
+
+  constructor(@InjectRepository(User) private userRepository: Repository<User>) {}
+
+}
